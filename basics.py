8b682922bed0b22fb610c492de1516edb5d4b938
MChang360686 <readingcyclist@gmail.com>
2023-11-01 19:10:41 -0400

    New project + password game update

---
+++ basics.py
@@ -0,0 +1,23 @@
+import time
+
+# Practice if/else elif statements
+
+
+number = int(input("Please enter a number"))
+
+print(type(number))
+
+if number == 5:
+    print("Number is 5")
+elif number == 6:
+    print("Number is 6")
+else:
+    print("No")
+
+# Ask: WHY don't we use these all the time?
+
+# Ask: What else can we use these for?
+# a) checking
+# b) replace try-catch statements.
+
+# Practice while loops
---
+++ python-games/game.py
@@ -0,0 +1,5 @@
+import pygame
+import time
+
+# Heavily based off of TWT's project
+# https://www.youtube.com/watch?v=waY3LfJhQLY
