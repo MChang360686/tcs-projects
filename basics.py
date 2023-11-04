e41a33f8f1bb2e384dc9ea7b4ed9a2a1ea5245d7
MChang360686 <readingcyclist@gmail.com>
2023-11-03 23:31:46 -0400

    updated basics and numberpractice

--- basics.py
+++ basics.py
@@ -15,9 +15,39 @@ else:
     print("No")
 
 # Ask: WHY don't we use these all the time?
+# Ans: BC we have to check EVERY statement
 
 # Ask: What else can we use these for?
 # a) checking
 # b) replace try-catch statements.
 
+# Practice for loops
+# Why or when would we use them?
+
+name = 'robert'
+
+for letter in name:
+    print(letter)
+
+for i in range(0, 5):
+    print(i)
+
+list = ['Bob', 'Rob', 'Tob']
+for i in range(0, 3):
+    print(list[i])
+
+for name in list:
+    print(name)
+
+# Review: Try making a list of items and then modifiying those items
+
+
 # Practice while loops
+# Like for loops but a a little different
+
+num = 0
+while num < 5:
+    print(num)
+    num+=1
+
+# Review: Try making another list of items and modify those items using a while loop
--- numberpractice.py
+++ numberpractice.py
@@ -7,6 +7,14 @@ name = 'robert'
 for letter in name:
     print(letter)
 
+for i in range(0, 5):
+    print(i)
+
+num = 0
+while num < 5:
+    print(num)
+    num+=1
+
 endtime = time.time()
 timeelapsed = endtime - starttime
 
