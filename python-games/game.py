5b04cfeb88c634e039cedb88eb32f82d6a65f331
MChang360686 <readingcyclist@gmail.com>
2023-11-05 23:37:16 -0500

    started pygame project

--- python-games/game.py
+++ python-games/game.py
@@ -1,5 +1,11 @@
 import pygame
 import time
+import random
 
 # Heavily based off of TWT's project
 # https://www.youtube.com/watch?v=waY3LfJhQLY
+pygame.font.init()
+
+# Create window
+
+WIDTH, HEIGHT = 1000, 1000
