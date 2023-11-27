d63f984805b84874821a32e7740d97cc2e75e941
MChang360686 <readingcyclist@gmail.com>
2023-11-26 23:14:41 -0500

    made change to keyaan-js switch statements

--- keyaan-js.js
+++ keyaan-js.js
@@ -2,13 +2,13 @@
 function switchStatement(a) {
     switch (a) {
         case 1:
-            console.log("1");
+            console.log("option 1");
             break;
         case 2:
-            console.log("2");
+            console.log("option 2");
             break;
         case 3:
-            console.log("3");
+            console.log("option 3");
             break;
         default:
             console.log("IDK")
