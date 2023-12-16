fd01b1cdf7daf9cfe01dcf81fd5428a6d6f6b3bc
MChang360686 <readingcyclist@gmail.com>
2023-12-15 22:13:13 -0500

    new project and edited turtle1

--- turtle-project1.py
+++ turtle-project1.py
@@ -5,63 +5,32 @@ screen.setup(1.0, 1.0)
 
 t = turtle.Turtle()
 
-"""for color in ["red", "green", "blue", "yellow"]:
-    t.color(color)
-    t.forward(50)
-    t.right(90)
-
-t.fillcolor("red")
-t.begin_fill()
-
-
-
-radius = 20
-t.circle(radius)
-
-for i in range(0, 3):
-    t.forward(100)
-    t.left(120)"""
-
 shape = input("Please enter a shape name")
 color =  input("Please enter a color")
 size = int(input("Please enter a number of pixels"))
 
-def drawShape(shape, color):
+def drawShape(shape, color, size):
     if shape == "circle":
         t.color(color)
-        t.circle(100)
+        t.circle(size)
     elif shape == "square":
         t.color(color)
         for i in range(0, 4):
-            t.forward(100)
+            t.forward(size)
             t.right(90)
     elif shape == "triangle":
         t.color(color)
         for i in range(0, 3):
-            t.forward(100)
+            t.forward(size)
             t.left(120)
     elif shape == "pentagon":
         t.color(color)
         for i in range(0, 5):
-            t.forward(100)
+            t.forward(size)
             t.left(72)
     else:
         print("I don't know that shape")
 
 
-drawShape(shape, color)
+drawShape(shape, color, size)
 t.screen.mainloop()
-
-"""shape = input("Please enter a shape name ")
-color = input("Please enter a color ")
-
-v = turtle.getscreen()
-t = turtle.Turtle()
-
-t.color(color)
-
-if shape == "square":
-    for x in range(4):
-        t.forward(10)
-        t.right(90)
-        x += 1"""
