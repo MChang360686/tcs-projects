f55845bd1eada469d9711e201483063cbc3df169
MChang360686 <readingcyclist@gmail.com>
2023-12-21 18:50:41 -0500

    started new projects.  Calculator needs fixing

--- html-dev/impossiblequiz/script.js
+++ html-dev/impossiblequiz/script.js
@@ -1,12 +1,16 @@
+let lives = 3;
+
 function reduceLives() {
-    let lives = Number(document.getElementById("lives").innerHTML);
-    return lives--;
+    return lives - 1;
 }
 
 function checkGameOver(num) {
+    console.log(num)
     if (num == 0) {
         return true;
     } else {
+        document.getElementById("lives").innerHTML = num.toString();
+        console.log(typeof num.toString());
         return false;
     }
 }
@@ -21,6 +25,15 @@ document.getElementById("helpbutton").addEventListener('click', function () {
 });
 
 document.getElementById("q1b1").addEventListener('click', function () {
+    let gameover = checkGameOver(reduceLives());
+    if (gameover == true) {
+        console.log("Game Over");
+    } else {
+        document.getElementById("lives").innerHTML = toString(lives);
+    }
+});
+
+document.getElementById("q1b2").addEventListener('click', function () {
     let lives = reduceLives();
     let gameover = checkGameOver(lives);
     if (gameover == true) {
@@ -28,11 +41,21 @@ document.getElementById("q1b1").addEventListener('click', function () {
     } else {
         document.getElementById("lives").innerHTML = toString(lives);
     }
-})
+});
 
 document.getElementById("q1b3").addEventListener('click', function () {
     var q2 = document.getElementById('q2');
     q2.style.display = 'block'
     var q1 = document.getElementById('q1');
     q1.style.display = 'none';
+});
+
+document.getElementById("q1b4").addEventListener('click', function () {
+    let lives = reduceLives();
+    let gameover = checkGameOver(lives);
+    if (gameover == true) {
+        console.log("Game Over");
+    } else {
+        document.getElementById("lives").innerHTML = toString(lives);
+    }
 });
