cbdf6cdf24d2ba869a63e87ab70729c9585224b9
MChang360686 <readingcyclist@gmail.com>
2023-12-23 10:03:04 -0500

    new project for kids

---
+++ adk.py
@@ -0,0 +1,19 @@
+
+list = [1, 2, 3, 4, 5]
+
+def printList(l):
+    for num in l:
+        num += 3
+        print(num)
+
+def compare(a, b):
+    if(a > b):
+        print("One Thing")
+    else:
+        print("Another Thing")
+
+"""for i in range(0, 5):
+    print(i)"""
+
+printList(list)
+compare(5, 8)
