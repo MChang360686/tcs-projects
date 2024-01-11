39860aed7c1f64fb37eda2962e6db366a2f52f6f
MChang360686 <readingcyclist@gmail.com>
2024-01-11 17:23:31 -0500

    fleshed out switch statement lesson a little more.

--- switchstatement.py
+++ switchstatement.py
@@ -1,2 +1,25 @@
+import random
 
 # review switch statements in Python
+
+num = random.choice([1, 2, 3, 4, 5, 6, 7, 8])
+
+match num:
+    case 1: 
+        print(num)
+    case 2:
+        print(num)
+    case 3:
+        print(num)
+    case 4:
+        print(num)
+    case 5:
+        print(num)
+    case 6:
+        print(num)
+    case 7:
+        print(num)
+    case 8:
+        print(num)
+    case _:
+        print("Not a number 1-8")
